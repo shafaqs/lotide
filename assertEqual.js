2ab4ce92649d1371c99dcb1859dd7f9bad382ee1
Shafaq Saud <shafaq.saud@gmail.com>
2022-09-24 13:32:49 -0400

    complete

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👻 "+"Assertion Passed: " + actual + "===" + expected);
+  }
+  else {
+    console.log("😵"+"Assertion Failed: " + actual + "!==" + expected);
+  }
+};
+//REST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
