d51cb4dabdafc82d8344e148390ed3412f6a7c99
Shafaq Saud <shafaq.saud@gmail.com>
2022-09-30 15:14:21 -0400

    for in loop saves the day

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👻 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`😵 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKeyByValue startes from here
+const findKeyByValue = function(obj, value){
+  for (let key in obj) {
+    if(obj[key] === value)
+    return key
+  }
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
