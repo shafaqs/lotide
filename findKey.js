5f182885424c038eff9881d61ecdb436325c7bbd
Shafaq Saud <shafaq.saud@gmail.com>
2022-10-05 17:59:42 -0400

    tested

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👻 Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`😵 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+// findKey starts here
+
+
+const findKey = function(obj, callback){
+  for (let i in obj) {
+    if (callback(obj[i])){
+      console.log(i)
+      return i
+    }
+  }
+  return undefined
+}
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual("noma", findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
